bc68ed173a498a1dde4192d87d3f59155c4f7f0f
git@github.com:evgenivanov1980/MyFinance.git <ivanov.evgen.v@gmail.com>
2022-10-18 19:25:42 +0500

    Create Client

---
+++ src/main/java/com/company/myfinance/entity/Client.java
@@ -0,0 +1,11 @@
+package com.company.myfinance.entity;
+
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.Entity;
+
+@JmixEntity
+@Entity
+public class Client extends User {
+
+}
---
+++ src/test/java/com/company/myfinance/MyFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.myfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
