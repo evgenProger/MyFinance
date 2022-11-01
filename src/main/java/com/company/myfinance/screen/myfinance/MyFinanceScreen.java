f99601c7da01b8e5b0d2f0fb91c5f5468fad39be
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2022-11-01 18:13:43 +0500

    Add Filter by Type and date

--- src/main/java/com/company/myfinance/screen/myfinance/MyFinanceScreen.java
+++ src/main/java/com/company/myfinance/screen/myfinance/MyFinanceScreen.java
@@ -4,13 +4,28 @@ import com.company.myfinance.entity.Client;
 import com.company.myfinance.entity.Transaction;
 import com.company.myfinance.entity.Type;
 import com.company.myfinance.entity.User;
+import io.jmix.core.DataManager;
 import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.action.BaseAction;
+import io.jmix.ui.component.Button;
+import io.jmix.ui.component.EntityComboBox;
 import io.jmix.ui.component.Label;
+import io.jmix.ui.component.Table;
+import io.jmix.ui.component.data.TableItems;
 import io.jmix.ui.model.CollectionContainer;
 import io.jmix.ui.model.CollectionLoader;
+import io.jmix.ui.model.DataContext;
 import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
 
 @UiController("MyFinanceScreen")
 @UiDescriptor("my-finance-screen.xml")
@@ -22,27 +37,35 @@ public class MyFinanceScreen extends Screen {
     @Autowired
     private CollectionContainer<Transaction> transactionsDc;
     @Autowired
-    private Label totalAmount;
-    @Autowired
     private CollectionLoader<Type> typesDl;
-
+    @Autowired
+    private Notifications notifications;
+    @Autowired
+    private EntityComboBox<Type> typeField;
+    @Autowired
+    private CollectionContainer<Type> typesDc ;
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
         transactionsDl.load();
         typesDl.load();
-        updateLabels();
     }
 
-//    @Subscribe(id = "transactionsDc", target = Target.DATA_CONTAINER)
-//    public void onTransactionsDcItemPropertyChange(InstanceContainer.ItemPropertyChangeEvent<Transaction> event) {
-//        updateLabels();
-//
-//    }
-
-    private void updateLabels() {
-        long completedAmount = transactionsDc.getItems().stream()
-                .mapToLong(a -> a.getTransfer_amount().longValue()).sum();
-        totalAmount.setValue("Total amount: " + completedAmount);
+    private void findByType() {
+        Type type = typeField.getValue();
+        List<Transaction> items = transactionsDc.getItems();
+        if (type == null) {
+            transactionsDl.load();
+            return;
+        }
+        List<Transaction> transactions = items.stream().filter(t -> t.getTypes().contains(type)).toList();
+        transactionsDc.setItems(transactions);
     }
+
+    @Subscribe("saveButton")
+    public void onSaveButtonClick(Button.ClickEvent event) {
+        findByType();
+    }
+
+
 }
--- src/main/java/com/company/myfinance/security/ClientRole.java
+++ src/main/java/com/company/myfinance/security/ClientRole.java
@@ -15,7 +15,7 @@ import io.jmix.securityui.role.annotation.ScreenPolicy;
 @ResourceRole(name = "ClientRole", code = "client-role", scope = "UI")
 public interface ClientRole {
 	@MenuPolicy(menuIds = {"BankAccount.browse", "Transaction_.browse", "Type_.browse", "Client.browse", "MyFinanceScreen"})
-	@ScreenPolicy(screenIds = {"BankAccount.browse", "Transaction_.browse", "BankAccount.edit", "LoginScreen", "MainScreen", "Transaction_.edit", "Type_.edit", "Type_.browse", "Client.browse", "MyFinanceScreen"})
+	@ScreenPolicy(screenIds = {"BankAccount.browse", "Transaction_.browse", "BankAccount.edit", "LoginScreen", "MainScreen", "Transaction_.edit", "Type_.edit", "Type_.browse", "Client.browse", "MyFinanceScreen", "ui_AddConditionScreen", "ui_GroupFilterCondition.edit", "ui_JpqlFilterCondition.edit", "ui_PropertyFilterCondition.edit", "ui_FilterConfigurationModel.fragment", "sec_RoleFilterFragment"})
 	void screens();
 	
 	@EntityAttributePolicy(entityClass = Type.class, attributes = "*", action = EntityAttributePolicyAction.MODIFY)
