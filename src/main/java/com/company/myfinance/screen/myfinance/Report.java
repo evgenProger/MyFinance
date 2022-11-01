b7c4efaada16dc25bd60f8df71daad7ef453df6d
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2022-11-01 21:40:07 +0500

    Rename Report screen

--- src/main/java/com/company/myfinance/screen/myfinance/Report.java
+++ src/main/java/com/company/myfinance/screen/myfinance/Report.java
@@ -1,35 +1,22 @@
 package com.company.myfinance.screen.myfinance;
 
-import com.company.myfinance.entity.Client;
 import com.company.myfinance.entity.Transaction;
 import com.company.myfinance.entity.Type;
-import com.company.myfinance.entity.User;
-import io.jmix.core.DataManager;
 import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.ui.Notifications;
-import io.jmix.ui.action.BaseAction;
 import io.jmix.ui.component.Button;
 import io.jmix.ui.component.EntityComboBox;
-import io.jmix.ui.component.Label;
-import io.jmix.ui.component.Table;
-import io.jmix.ui.component.data.TableItems;
 import io.jmix.ui.model.CollectionContainer;
 import io.jmix.ui.model.CollectionLoader;
-import io.jmix.ui.model.DataContext;
-import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 
-@UiController("MyFinanceScreen")
+@UiController("Report")
 @UiDescriptor("my-finance-screen.xml")
-public class MyFinanceScreen extends Screen {
+public class Report extends Screen {
     @Autowired
     private CurrentAuthentication currentAuthentication;
     @Autowired
